5a8036d4864ce41d4dbf55d735e6dff547bee5df
geno <yvesgeno@outlook.com>
2023-05-22 07:13:03 +0200

    simple shell

--- builtins.c
+++ builtins.c
@@ -45,12 +45,10 @@ int builtin_env(char **args)
  */
 int builtin_exit(char **args)
 {
-	/* exit with status */
 	if (args[1])
 	{
 		return (_atoi(args[1]));
 	}
-	/* exit success */
 	else
 	{
 		return (0);
